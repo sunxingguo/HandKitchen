4d037898ba6467333446e62d126aa12a1d5e844e
qianfeng <ms@dingtaladulutalahuluwa.local>
2015-07-06 14:18:38 +0800

    Initial Commit

---
+++ HandKitchen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HandKitchen
+//
+//  Created by qianfeng on 15-7-6.
+//  Copyright (c) 2015年 qianfeng-sunxingguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
